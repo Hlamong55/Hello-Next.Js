5f5f83a05b24be2a5224c6b9d6a92460d3ba8f1a
Hlamong55 <bijoymarma55@gmail.com>
2026-03-18 23:02:07 +0600

    created about page & its nested routes

---
+++ src/app/about/contact/page.jsx
@@ -0,0 +1,12 @@
+import Title from '@/components/Title';
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <Title>Contact No: 01818470577</Title>
+        </div>
+    );
+};
+
+export default Contact;

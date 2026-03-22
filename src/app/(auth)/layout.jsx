aa62ecca9c41cf11468d8dcbdb056e117922ecff
Hlamong55 <bijoymarma55@gmail.com>
2026-03-22 22:47:14 +0600

    implement all segments in a complex route

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const AuthLayout = ({ children }) => {
     return (
-        <div className='flex gap-5 '>
+        <div className='flex gap-5 min-h-screen'>
             <div className='flex-2'>
                 {children}
             </div>
 
-            <div className='flex-1 min-h-screen bg-blue-700 p-5'>
-                <h2 className='text-5xl font-bold  text-white'>Welcome for Authentication</h2>
+            <div className='flex-1 flex justify-center items-center bg-blue-700 p-5'>
+                <h2 className='text-5xl text-center font-bold  text-white'>Welcome for Authentication</h2>
             </div>
         </div>
     );

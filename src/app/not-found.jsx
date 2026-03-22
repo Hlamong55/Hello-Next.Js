450235771deed7e9f4791a76af034ae08d23606b
Hlamong55 <bijoymarma55@gmail.com>
2026-03-22 23:37:26 +0600

    implement active links & not found page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound404 = () => {
+    return (
+        <div className='min-h-screen flex flex-col justify-center items-center'>
+
+            <h1 className='text-6xl font-extrabold text-sky-600'>404</h1>
+            <h2 className='text-5xl font-extrabold'>Not Found !!</h2>
+        </div>
+    );
+};
+
+export default NotFound404;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,24 +2,25 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react';
+import NavLink from './NavLink';
 
 const Header = () => {
   const pathname = usePathname();
   if (pathname.startsWith("/dashboard")) return <></>;
-  
+
     return (
         <header className="px-8 py-4 border-b-2 border-gray-500 flex justify-between items-center flex-wrap">
           <Link href="/" className="text-xl font-extrabold">🐞Dev Story</Link>
 
           <nav className='space-x-5 '>
-            <Link href="/about">About Us</Link>
-            <Link href="/tutorials">Tutorials</Link>
-            <Link href="/stories">Stories</Link>
+            <NavLink href="/about">About Us</NavLink>
+            <NavLink href="/tutorials">Tutorials</NavLink>
+            <NavLink href="/stories">Stories</NavLink>
           </nav>
 
           <nav className='space-x-5'>
-            <Link href="/login">Login</Link>
-            <Link href="/register">Register</Link>
+            <NavLink href="/login">Login</NavLink>
+            <NavLink href="/register">Register</NavLink>
           </nav>
         </header>
     );

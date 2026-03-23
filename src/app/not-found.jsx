60feac08286f312116c3e286a419062d3d7e1324
Hlamong55 <bijoymarma55@gmail.com>
2026-03-23 21:47:30 +0600

    implement loading animation for routing

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const NotFound404 = () => {
@@ -6,6 +7,10 @@ const NotFound404 = () => {
 
             <h1 className='text-6xl font-extrabold text-sky-600'>404</h1>
             <h2 className='text-5xl font-extrabold'>Not Found !!</h2>
+
+            <Link 
+            className='mt-8 bg-sky-600 px-4 py-2.5 rounded-lg text-white font-semibold'
+            href={"/"}>Go Home</Link>
         </div>
     );
 };
---
+++ src/app/stories/[id]/loading.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div className='min-h-screen flex justify-center items-center text-7xl'>
+
+            <h1>L</h1> <span className='animate-spin'>🎯</span>
+            <h2 className='text-sky-500'>ADING</h2>
+     
+        </div>
+    );
+};
+
+export default loading;
